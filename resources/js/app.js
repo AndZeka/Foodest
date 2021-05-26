cf80c691341dfd49b7b496494295abb55a3999f7
AndZeka <andizeka@hotmail.com>
2021-05-26 10:07:32 +0200

    Adding vuejs in the project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,12 @@
+import Vue from 'vue'
+window.Vue = Vue;
+
 require('./bootstrap');
 
 require('alpinejs');
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+const app = new Vue({
+  el: '#app',
+});
