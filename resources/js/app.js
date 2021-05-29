f2a81a49c58cdd566f54298432c5c588077a80d5
AndZeka <andizeka@hotmail.com>
2021-05-29 10:08:49 +0200

    Admin Dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,7 @@ require('./bootstrap');
 
 require('alpinejs');
 
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-const app = new Vue({
-  el: '#app',
-});
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// const app = new Vue({
+//   el: '#app',
+// });
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,7 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
-    ]).vue({version:2});
+    ]).vue({version:2}).sass('resources/sass/app.scss', 'public/css');
 
 if (mix.inProduction()) {
     mix.version();
