da71fc4d4106e84bedeb169ef344d854e6fb4461
AndZeka <andizeka@hotmail.com>
2021-05-31 21:03:09 +0200

    Active links

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,22 @@
-import Vue from 'vue'
-window.Vue = Vue;
 
 require('./bootstrap');
 
 require('alpinejs');
+import Vue from 'vue'
+window.Vue = Vue;
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+    { path: '/profile', component: require('./components/Profile.vue').default }
+]
+
+const router = new VueRouter({routes, mode: 'history'});
 
 // Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-// const app = new Vue({
-//   el: '#app',
-// });
+const app = new Vue({
+  el: '#app',
+  router
+});
